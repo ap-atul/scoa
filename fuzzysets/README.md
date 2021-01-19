52405e0768a60f9f755626a2906a0f0219f26c2d
AP-Atul <atulpatare99@gmail.com>
2021-01-19 10:22:42 +0530

    set operations added

---
+++ fuzzysets/README.md
@@ -0,0 +1,33 @@
+## Fuzzy Sets
+The values in fuzzy sets are unclear, like normal set the value
+are either present or absent, but in fuzzy sets the value can have 
+some percent value ranging from (0 -> 1) closer to 0 denotes absent,
+closer to 1 denotes present.
+
+
+## Usage
+
+```python
+# delcare a set
+A = FuzzySet( [ ('a', 1), ('b', 0.5), ('c', 0.6) ] )
+
+# or with dictionary
+B = FuzzySet( {'d': 0.8, 'e': 0.01, 'f': 0.4})
+
+# set operations
+# union
+out = A.union(B)
+
+# intersection
+out = A.inter(B)
+
+# complement
+out = A.comp()
+
+# difference
+out = A.diff(B)
+
+```
+
+# TODO
+* Relations
---
+++ fuzzysets/fuzzy.py
@@ -0,0 +1,60 @@
+class FuzzySet:
+    def __init__(self, data):
+        self._set = dict()
+
+        if isinstance(data, dict):
+            self._set = data
+            return
+
+        if not isinstance(data, (list, set)):
+            raise Exception("Data should be type of list or set")
+
+        if not isinstance(data[0], (tuple, list, set)):
+            raise Exception("Each element in the data should be set, tuple or list")
+
+        for item, val in data:
+            if item in self._set:
+                raise Exception("Sets cannot have duplicate values")
+
+            self._set[item] = val
+
+    def __getitem__(self, key):
+        return self._set[key]
+
+    def __repr__(self):
+        return str(self._set)
+
+    def union(self, other):  # max(deg(A), deg(B))
+        ret = dict()
+
+        for akey, bkey in zip(self._set, other._set):
+            aval , bval = self[akey], other[bkey]
+
+            key, val = (akey, aval) if (aval > bval) else (bkey, bval)
+            ret[key] = val
+
+        return FuzzySet(ret)
+
+    def inter(self, other):  # min(deg(A), deg(B))
+        ret = dict()
+
+        for akey, bkey in zip(self._set, other._set):
+            aval , bval = self[akey], other[bkey]
+
+            key, val = (akey, aval) if (aval < bval) else (bkey, bval)
+            ret[key] = val
+
+        return FuzzySet(ret)
+
+    def comp(self):  # 1 - deg(A)
+        for key in self._set:
+            self._set[key] = 1 - self._set[key]
+
+        return self
+
+    def diff(self, other):  # min(deg(A), 1 - deg(B))
+        return self.inter(other.comp())
+
+
+
+
