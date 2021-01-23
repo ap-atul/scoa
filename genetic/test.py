910c306ec6ff534bfba79feb39756ae1d3271836
AP-Atul <atulpatare99@gmail.com>
2021-01-23 23:06:41 +0530

    test added

--- genetic/test.py
+++ genetic/test.py
@@ -1,13 +1,6 @@
 from genetic import *
 
-## TODO
-## 1. Generate random populatio of genome (tuple (x, y))
-## 2. Cross over
-## 3. Mutation (min = -10, max = 10)
-## 4. tournament selection (select random sol, add only the fittest)
-## 5. run tour on 2, use them to cross over, then mutate on new pop
-
-sol, fit = run_genetic(10, 100)
+sol, fit = run_genetic(pop_size=100, gen_size=10, disp=True)
 
 print()
 print(f"Fitness of the solution :: {rosenbrock(sol)}")
