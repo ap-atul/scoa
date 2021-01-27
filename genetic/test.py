7fc1e344223d11cafe74c094b49797966cd5114f
AP-Atul <atulpatare99@gmail.com>
2021-01-27 22:09:31 +0530

    clean up

--- genetic/test.py
+++ genetic/test.py
@@ -2,8 +2,5 @@ from genetic import *
 
 sol, fit = run_genetic(pop_size=100, gen_size=20, disp=True)
 
-print()
 print(f"Fitness of the solution :: {rosenbrock(sol)}")
 print(f"Solution after evolution :: {sol}")
-
-
